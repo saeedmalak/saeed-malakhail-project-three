3caa067df4265e3a047c3156e8ddd153b48cda86
saeedmalak <makhh2001@gmail.com>
2020-11-11 17:35:47 -0500

    MVP project completed ::relaxed:

--- script.js
+++ script.js
@@ -12,95 +12,122 @@ guessApp.answers = {
   word: ["kite", "coding"]
 }
 
+let myAnswer;
+let timerVar;
+let seconds = 10;
+
 const randomizer = function (array) {
   return Math.floor (Math.random () * array.length);
+}
+
+//Start GuessApp
+guessApp.init = function () {
+  $winningPage = $(".winner");
+  $playPage = $(".play");
+  $gameOverPage = $(".game-over");
+  $instructionsPage = $("header");
+
+  $timer = $("#timer");
+  $spaces = $("#spaces");
+
+  $image = $("#image");
+
+  $winningPage.hide();
+  $playPage.hide();
+  $gameOverPage.hide();
+
+  guessApp.displayInfo();
+}
+
+// Display the randomly selected image and "_" onto the screen
+guessApp.displayInfo = function () {
+  let underScores = "";
+  let answerArray = guessApp.answers.word;
+
+  let randomAnswerIndex = randomizer(answerArray);
+  myAnswer = answerArray[randomAnswerIndex];
+
+  $image.attr("src", `./assets/image${randomAnswerIndex}.jpg`);
+
+  for (let i = 0; i < myAnswer.length; i++) {
+    underScores = underScores + "_" + " ";
+    $spaces.text(`${underScores}`);
+  }
+
+  guessApp.findMatch();
+
+}
+
+guessApp.countTimer = function () {
+  seconds--;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (seconds <= 0) {
+    clearInterval(timerVar);
+    seconds = 10;
+    $timer.html("00");
+    $playPage.hide();
+    $gameOverPage.show();
+  }
+  $timer.html(`${seconds} Sec...`);
+
+}
+
+guessApp.findMatch = function () {
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+
+    let $userInput = $("#guess").val();
+
+    if ($userInput === myAnswer) {
+      clearInterval(timerVar);
+      seconds = 10;
+      $timer.html("00");
+      $playPage.hide();
+      $winningPage.show();
+    }
+
+    $("#guess").val("");
+  })
+
+  guessApp.buttonEventListeners();
+
+}
+
+guessApp.buttonEventListeners = function () {
+  $("#play-again").on("click", function () {
+
+    $gameOverPage.hide();
+    timerVar = setInterval(guessApp.countTimer, 1000);
+    $playPage.show();
+
+  });
+
+  $("#restart").on("click", function () {
+
+    $winningPage.hide();
+    timerVar = setInterval(guessApp.countTimer, 1000);
+    $playPage.show();
+
+  });
+
+  $(".start").on("click", function () {
+
+    timerVar = setInterval(guessApp.countTimer, 1000);
+    $instructionsPage.hide();
+    $playPage.show();
+
+  });
 }
 
 
 
 
 
-$(document).ready(function () {
+$ (function() {
 
-  $(".winner").hide();
-  $(".play").hide();
-  $(".game-over").hide();
-
-  let timerVar;
-  let seconds = 10;
-  let underScores = "";
-
-  let randomAnswerIndex = randomizer(guessApp.answers.word);
-
-  let answer = guessApp.answers.word[randomAnswerIndex];
-
-
-  $("#image").attr("src", `./assets/image${randomAnswerIndex}.jpg`);
-
-  for (let i=0; i< answer.length; i++) {
-
-    underScores = underScores + "_" + " ";
-    $("#spaces").text(`${underScores}`);
-
-  }
-
-
-  $("form").on("submit", function (event) {
-    event.preventDefault();
-
-    let userInput = $("#guess").val();
-
-    if (userInput === answer) {
-      clearInterval(timerVar);
-      seconds = 10;
-      $("#timer").html("00");
-      $(".play").hide();
-      $(".winner").show();
-    }
-    
-    $("#guess").val("");
-
-    console.log(userInput);
-
-  })
-
-  $("#play-again").on("click", function () {
-
-    $(".game-over").hide();
-    timerVar = setInterval(countTimer, 1000);
-    $(".play").show();
-
-  });
-
-  $("#restart").on("click", function () {
-
-    $(".winner").hide();
-    timerVar = setInterval(countTimer, 1000);
-    $(".play").show();
-
-  });
-
-  $(".start").on("click", function () {
-
-    timerVar = setInterval(countTimer, 1000);
-    $("header").hide();
-    $(".play").show();
-    
-  });
-
-  const countTimer = function () {
-    seconds--;
-    if (seconds < 10){
-      seconds = "0" + seconds;}
-    if (seconds <= 0) {
-      clearInterval(timerVar);
-      seconds = 10;
-      $("#timer").html("00");
-      $(".play").hide();
-      $(".game-over").show();
-    }
-    $("#timer").html(`${seconds} Sec...`);
-  }
-
+  guessApp.init();
 
 });
