8201988a165c23d3585406d2abc12b71bc7cc8a1
saeedmalak <makhh2001@gmail.com>
2020-11-14 01:24:59 -0500

    Animations completed, font stylings and added HOME button:fire:

--- script.js
+++ script.js
@@ -19,23 +19,31 @@ let myAnswer = answerArray[randomAnswerIndex];
 
 // Global variables, the timerID and the number of seconds the user has to successfully guess the word!
 let timerVar;
-let seconds = 10;
+let seconds = 12;
 
 //Start GuessApp
 guessApp.init = function () {
-  $winningPage = $(".winner");
-  $playPage = $(".play");
-  $gameOverPage = $(".game-over");
+  // cache all jquery selectors 
   $instructionsPage = $("header");
+  $startBtn = $(".start");
+  $playPage = $(".play");
+  $homePageBtn = $(".home-page-btn");
+
+  $resultsPage = $(".results");
+  $message = $(".message");
+  $resultsBtn = $(".results-btn");
+  $resultsIcons = $(".results-icons");
 
   $timer = $("#timer");
   $spaces = $("#spaces");
 
   $image = $("#image");
+  $("#guess").val("");
 
-  $winningPage.hide();
+  $(".flex-timer").removeClass("timer-alert");
+
+  $resultsPage.hide();
   $playPage.hide();
-  $gameOverPage.hide();
 
   guessApp.displayInfo(randomAnswerIndex, myAnswer);
   guessApp.findMatch();
@@ -56,21 +64,60 @@ guessApp.displayInfo = function (randomAnswerIndex, myAnswer) {
 }
 
 // Start the time interval and go down by 1 per second!
+// Update H2, Button and icons accordingly if user does NOT win
 guessApp.countTimer = function () {
+  
+  $('.results-icons').empty();
   seconds--;
-  $('.game-over-icons').empty();
+
   if (seconds < 10) {
+    $(".flex-timer").addClass("timer-alert"); // animate the timer to get the user's attention when timer is less than 10 seconds!
     seconds = "0" + seconds;
   }
   if (seconds <= 0) {
     clearInterval(timerVar);
     guessApp.displayLosingIcons();
-    seconds = 10;
+    seconds = 15;
     $timer.html("00");
+    $(".flex-timer").removeClass("timer-alert");
     $playPage.hide();
-    $gameOverPage.show();
+    $resultsPage.show();
+    $message.addClass("animate__animated animate__backInDown animate__slow");
+    $message.html("GAME OVER!");
+    $resultsBtn.html("Play Again");
+    $resultsPage.show();
   }
   $timer.html(`${seconds} Sec...`);
+
+}
+
+// Check to see if the user has answered correctly!
+// Update H2, Button and icons accordingly if user wins
+guessApp.findMatch = function () {
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+
+    $('.results-icons').empty();  // empty all icons 
+
+    let $userInput = $("#guess").val();
+
+    if ($userInput === myAnswer) {
+      clearInterval(timerVar);
+      guessApp.displayWinningIcons();
+      seconds = 15;
+      $timer.html("00");
+      $(".flex-timer").removeClass("timer-alert");
+      $playPage.hide();
+      $message.addClass("animate__animated animate__backInDown animate__slow");
+      $message.html("You Won! Congrats!");
+      $resultsBtn.html("Restart");
+      $resultsPage.show();
+    }
+
+    $("#guess").val(""); // empty the user input field after a wrong answer
+
+  })
 
 }
 
@@ -79,19 +126,19 @@ guessApp.displayWinningIcons = function () {
   // Display the '22' happy icons animating on the screen when user wins the game :)
   for (i = 1; i <= 22; i++) {
 
-    if (i>=1 && i<=11) {
+    if (i >= 1 && i <= 11) {
       if (i == 4 || i == 8) {
-        $(".winning-icons").append(`<li><i class="far fa-smile-beam animate__animated animate__backOutUp animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="far fa-smile-beam animate__animated animate__backOutUp animate__infinite"></i></li>`);
       }
       else
-        $(".winning-icons").append(`<li><i class="far fa-smile animate__animated animate__backOutUp animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="far fa-smile animate__animated animate__backOutUp animate__infinite"></i></li>`);
     }
     else if (i >= 12 && i <= 22) {
       if (i == 15 || i == 19) {
-        $(".winning-icons").append(`<li><i class="far fa-smile-beam animate__animated animate__backOutDown animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="far fa-smile-beam animate__animated animate__backOutDown animate__infinite"></i></li>`);
       }
       else
-        $(".winning-icons").append(`<li><i class="far fa-smile animate__animated animate__backOutDown animate__infinite"></i></li>`); 
+        $resultsIcons.append(`<li><i class="far fa-smile animate__animated animate__backOutDown animate__infinite"></i></li>`);
     }
   }
 }
@@ -103,87 +150,56 @@ guessApp.displayLosingIcons = function () {
 
     if (i >= 1 && i <= 11) {
       if (i == 4 || i == 8) {
-        $(".game-over-icons").append(`<li><i class="fas fa-fire animate__animated animate__backOutUp animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="fas fa-fire animate__animated animate__backOutUp animate__infinite"></i></li>`);
       }
       else
-        $(".game-over-icons").append(`<li><i class="fas fa-sad-tear animate__animated animate__backOutUp animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="fas fa-sad-tear animate__animated animate__backOutUp animate__infinite"></i></li>`);
     }
     else if (i >= 12 && i <= 22) {
       if (i == 15 || i == 19) {
-        $(".game-over-icons").append(`<li><i class="fas fa-fire animate__animated animate__backOutDown animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="fas fa-fire animate__animated animate__backOutDown animate__infinite"></i></li>`);
       }
       else
-        $(".game-over-icons").append(`<li><i class="fas fa-sad-tear animate__animated animate__backOutDown animate__infinite"></i></li>`);
+        $resultsIcons.append(`<li><i class="fas fa-sad-tear animate__animated animate__backOutDown animate__infinite"></i></li>`);
     }
   }
 }
 
+// Reset all game settings 
+const resetGameSettings = function () {
+  // select a new random index, word and image
+  randomAnswerIndex = randomizer(answerArray);
+  myAnswer = answerArray[randomAnswerIndex];
+  guessApp.displayInfo(randomAnswerIndex, myAnswer);
 
-// Check to see if the user has answered correctly!
-guessApp.findMatch = function () {
-
-  $("form").on("submit", function (event) {
-    event.preventDefault();
-
-    $('.winning-icons').empty();
-
-    let $userInput = $("#guess").val();
-
-    if ($userInput === myAnswer) {
-      clearInterval(timerVar);
-      guessApp.displayWinningIcons();
-      seconds = 10;
-      $timer.html("00");
-      $playPage.hide();
-      $("#winner-message").addClass("animate__animated animate__backInDown animate__slow");
-      $winningPage.show();
-    }
-
-    $("#guess").val("");
-  })
-
+  // remove animated classes from H2 to prevent JS being loaded onto the screen (looks weird so thanks to removeClass & addClass!)
+  $message.removeClass("animate__animated animate__backInDown animate__slow");
+  $resultsPage.hide();
 }
 
-// Display the relevant 'sections' depending on which button is clicked: Start Game, Game Won, or Game Lost
+// Display the relevant 'sections' depending on which button is clicked: Start Game or Results Button (Game Won or Game Lost)
 guessApp.buttonEventListeners = function () {
-  $("#play-again").on("click", function () {
-
-    // select a new random index, word and image
-    randomAnswerIndex = randomizer(answerArray); 
-    myAnswer = answerArray[randomAnswerIndex];
-    guessApp.displayInfo(randomAnswerIndex, myAnswer);
-
-    $gameOverPage.hide();
+  // Show the play area page when user hits "RESTART"
+  $resultsBtn.on("click", function () {
+    resetGameSettings();
+    // start the timer and show the Play Area
     timerVar = setInterval(guessApp.countTimer, 1000);
     $playPage.show();
-    $('#guess').focus(); // place the cursor inside the input box when page is loaded
-
-    
-
-  });
-
-  $("#restart").on("click", function () {
-
-    // select a new random index, word and image
-    randomAnswerIndex = randomizer(answerArray);
-    myAnswer = answerArray[randomAnswerIndex];
-    guessApp.displayInfo(randomAnswerIndex, myAnswer);
-
-    $("#winner-message").removeClass("animate__animated animate__backInDown animate__slow");
-    $winningPage.hide();
-    timerVar = setInterval(guessApp.countTimer, 1000);
-    $playPage.show();
+    // place the cursor inside the inputbox when page loads to give user good experience!
     $('#guess').focus();
-
   });
+
+  // Show the home instructions page when user hits "HOME"
+  $homePageBtn.on("click", function () {
+    resetGameSettings();
+    $instructionsPage.show();
+  })
 
   $(".start").on("click", function () {
-
     timerVar = setInterval(guessApp.countTimer, 1000);
     $instructionsPage.hide();
     $playPage.show();
     $('#guess').focus();
-
   });
 }
 
